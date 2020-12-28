09b621ee1c7c9ffd7a6d40f14cf9dddbdaf2d4d7
ValeriyGor <valeriy.gorbachevskiy@gmail.com>
2020-12-28 22:16:47 +0200

    classWork14

---
+++ classWork13/js/script.js
@@ -0,0 +1,11 @@
+let str = 'Hello world from SCRIPT.JS!';
+
+//alert(str);
+
+let allowTea = confirm('Будете чай?');
+
+console.log(allowTea)
+
+let age = prompt('Ваш возраст:');
+
+console.log(age);
---
+++ classWork14/script.js
@@ -0,0 +1,76 @@
+//let n = prompt('Number');
+////
+////
+////console.log(2 * 6 * ++n); //2 
+//
+//n *= 5; // n = n * 5
+//
+//let s = 'test ' + n;
+//s += '\n'
+//s += 'Test: ' + n;
+//console.log(s, n)
+//
+//
+
+//let a = 'abck',
+//		b = 'abda';
+//
+//
+//console.log(null == undefined);
+//console.log(null == 25);
+//console.log(null == 'tre');
+//console.log(null === undefined);
+//
+
+//let age = null;
+// 
+//age = prompt('Your age'); 0 "" undefined '5t' null
+//
+//let salary = prompt('Your salary');
+//
+//if(salary >= 500) {
+//	console.log('Cool');
+//} else if(salary < 200) {
+//	console.log('Very BAD')
+//} else {
+//	console.log('Bad');
+//}
+
+
+//let count = prompt('Кол-во яблок');
+////let str = null;
+////
+////if(count == 1){
+////	str = 'apple';
+////} else {
+////	str = 'apples';
+////}
+//
+//let str = count == 1 ? 'apple' : 'apples'
+//
+//console.log(count + ' ' + str)
+
+
+//let a = prompt('Введите оценку');
+//
+//if(a >= 90 && a <= 100){
+//	console.log('Отлично');
+//} else if(a >= 70 && a < 90){
+//	console.log('Хорошо')
+//} else if (a >= 62 && a < 70){
+//	console.log("Ну троечка")
+//} else {
+//	console.log('поднажми, а то отчислят!')
+//}
+
+let password = '';
+password = prompt('Введите pass');
+
+
+if(!password){
+	console.log('Не валидное значение')
+} else{
+	do();
+}
+
+
