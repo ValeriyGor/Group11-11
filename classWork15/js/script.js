9c7139d71957ed00392c0911f0db6e3e42b13b4a
ValeriyGor <valeriy.gorbachevskiy@gmail.com>
2021-01-25 22:01:46 +0200

    classWork15

---
+++ classWork15/js/script.js
@@ -0,0 +1,115 @@
+//let name = prompt('Enter your name')
+//let age = +prompt('Enter your age')
+//
+//console.log(name)
+//console.log(age + 6) // 6 + 6 = 12
+
+
+//let res = name.length > 6 ? '>6' : '<6';
+//
+//console.log(res);
+
+//let city = 'Kiev';
+//
+//if(city === 'Kiev' || city === 'Baku'){
+//	alert('показываем товары для самовывоза')
+//} else {
+//	alert('скрываем товары для самовывоза')
+//}
+//
+//let isAuth = false;
+
+//if(!isAuth){
+//	console.log('test')
+//}
+
+//
+//let age = +prompt('Enter your age')
+//
+//switch(true){
+//	case (age >= 0 && age < 12): 
+//		console.log('Baby')
+//		break;
+//	case (age >= 12 && age < 20): 
+//		console.log('Teenager')
+//		break;
+//	case (age >= 20 && age < 28): 
+//		console.log('Зрелый')
+//		break;
+//	default: 
+//		console.log('Такого занчения нету')		
+//		break;		
+//}
+//
+//
+
+//
+//let i = 9;
+
+
+//while(i < 5){
+//	console.log(i++);
+//}
+
+//let sendSMS = false;
+//
+//do{	
+//	// Код отправки смс
+//	console.log('');
+//} while (!sendSMS)
+//
+//console.log(i)
+
+//for(let i = 0; i < 12; i++){
+//	let res = i%2 ? 'Непарное число: ' : 'Парное число: ';
+//	console.log(res + i);
+//}
+
+// i%2 == 0
+//Парное число: 0
+//Непарное число: 1
+//Парное число: 2
+//Непарное число: 3
+//...
+//Непарное число: 11
+
+//let n = 12.345;
+//
+//console.log(n.toFixed(2))
+//console.log(n.toFixed(0))
+//console.log(n.toFixed(1))
+//console.log(12.345.toFixed(1))
+//console.log(12..toFixed(10))
+//
+//console.log('Hello \nworld')
+//
+//console.log("Hello world. I'm programmer")
+
+//let str ='javaScript';
+//
+//let index = str.indexOf('Script');
+//console.log('Начало на ' + index + ' позиции');
+//
+//let dateTime = '2021-01-25 19:52:58'
+//let date = dateTime.substr(0, 10);
+//let time = dateTime.substring(11, 16);
+//
+//console.log(date);
+//console.log(time)
+//
+//console.log(+'458')
+//console.log(+'458e') // 458
+//console.log(parseFloat('458.32')) // 458
+//
+//console.log(Math.round(3.5))
+
+let cube1 = Math.floor(Math.random() * (7 - 1) + 1);
+let cube2 = Math.floor(Math.random() * (7 - 1) + 1);
+console.log("первый кубик выпал с номером: " + cube1)
+console.log("второй кубик выпал с номером: " + cube2)
+
+
+
+
+
+
